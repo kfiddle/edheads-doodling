48166eee9442a02206cda612330361dae6153244
kfiddle <kenjfiddle@gmail.com>
2021-06-11 14:05:01 -0400

    plane flight

--- src/components/airplane/Airplane.js
+++ src/components/airplane/Airplane.js
@@ -1,13 +1,18 @@
+import { useState, useEffect } from "react";
 
-
-import plane from '../../assets/plane.svg';
-import styles from './Airplane.module.css';
-
+import plane from "../../assets/plane.svg";
+import styles from "./Airplane.module.css";
 
 const Airplane = () => {
+  const [planePosition, setPlanePosition] = useState(25);
 
-    return <img src={plane} className={styles.plane}></img>
+  useEffect(() => {
+    setTimeout(() => {
+      setPlanePosition(-15);
+    }, 100);
+  }, []);
 
+  return <img src={plane} className={styles.plane} style={{transform: `translateX(${planePosition}vw)`}}></img>;
 };
 
 export default Airplane;
