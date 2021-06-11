c80180cad640cc0ecc909608425bfac7a3eaefcf
kfiddle <kenjfiddle@gmail.com>
2021-06-11 17:45:33 -0400

    chat boxes

---
+++ src/components/textBox/TextBox.js
@@ -0,0 +1,12 @@
+
+
+import styles from './TextBox.module.css';
+
+const TextBox = props => {
+
+
+    return <div className={`${styles.textBox} ${styles[props.direction]}`}>{props.children}</div>
+
+};
+
+export default TextBox;
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,10 +1,16 @@
+import TextBox from "../textBox/TextBox";
+
 import robot from "../../assets/placeholderRobot.svg";
 import styles from "./Welcome.module.css";
 
 const Welcome = (props) => {
   return (
     <div className={styles.welcomeDiv}>
-        <div className={styles.greetingDiv}></div>
+      {/* <div className={styles.greetingDiv}>
+        <TextBox direction={"right"} />
+      </div> */}
+
+      <TextBox direction={"right"}>Hello I'm a Lorem Ipsum thingie, even though I don't really know Latin.</TextBox>
       <div className={styles.robotDiv}>
         <img src={robot} className={styles.robot}></img>
       </div>
