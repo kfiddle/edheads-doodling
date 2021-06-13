3f82d3e5d9fb1427407910531ea356d9d16217d9
kfiddle <kenjfiddle@gmail.com>
2021-06-13 14:03:11 -0400

    explore games button

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -6,11 +6,10 @@ import styles from "./Welcome.module.css";
 const Welcome = (props) => {
   return (
     <div className={styles.welcomeDiv}>
-      {/* <div className={styles.greetingDiv}>
-        <TextBox direction={"right"} />
-      </div> */}
-
-      <TextBox direction={"left"}>Hello I'm a Lorem Ipsum thingie, even though I don't really know Latin.</TextBox>
+      <TextBox direction={"right"}>
+        Hello I'm a Lorem Ipsum thingie, even though I don't really know Latin.
+        <button className={styles.buttonToTheGames}>Explore Our Games!</button>
+      </TextBox>
       <div className={styles.robotDiv}>
         <img src={robot} className={styles.robot}></img>
       </div>
