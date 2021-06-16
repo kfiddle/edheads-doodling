0ef69bb4913e9e057a57e25e3d5aa226c3a5d2ce
kfiddle <kenjfiddle@gmail.com>
2021-06-16 17:25:38 -0400

    side panel orange

--- src/components/aboutUsPanel/AboutUsPanel.js
+++ src/components/aboutUsPanel/AboutUsPanel.js
@@ -1,30 +1,40 @@
+import { useState, useEffect } from "react";
 
-import { useState, useEffect } from 'react';
+import TextBox from "../textBox/TextBox";
+import styles from "./AboutUsPanel.module.css";
 
-import TextBox from '../textBox/TextBox';
-import styles from './AboutUsPanel.module.css';
+const AboutUsPanel = (props) => {
+    const firstPhase =
+    "linear-gradient(to Right, orangeRed, rgb(197, 110, 3), orange, orange, orange, orange )";
+  const secondPhase = "orange";
 
-
-const AboutUsPanel = props => {
-    const [slidePosition, setSlidePosition] = useState(100);
-
-    useEffect(() => {
-        setTimeout(()=> { setSlidePosition(0) }, 20);
-
-
-    }, [])
+  const [slidePosition, setSlidePosition] = useState(100);
+  const [color, setColor] = useState(firstPhase);
 
 
-    return <div className={styles.aboutUsDiv} style={{transform: `translateX(${slidePosition}vw)`}}>
+ 
 
-    <TextBox className={styles.textBox}><h1>Don't worry, this will have actual content soon.</h1>
-    <p>Plus, it won't be so orange.</p>
-    </TextBox>
+  useEffect(() => {
+    setTimeout(() => {
+      setSlidePosition(0);
+    }, 20);
 
+    setTimeout(() => {
+      setColor(secondPhase);
+    }, 800);
+  }, []);
 
+  return (
+    <div
+      className={styles.aboutUsDiv}
+      style={{ transform: `translateX(${slidePosition}vw)`, background:color }}
+    >
+      <TextBox className={styles.textBox}>
+        <h1>Don't worry, this will have actual content soon.</h1>
+        <p>Plus, it won't be so orange.</p>
+      </TextBox>
     </div>
-
-
+  );
 };
 
 export default AboutUsPanel;
--- src/components/topNavBar/NavBarButton.js
+++ src/components/topNavBar/NavBarButton.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 
 import styles from './NavBarButton.module.css';
 
 const NavBarButton = props => {
+    const [buttonHeight, setButtonHeight] = useState(3)
 
     const clicked = () => {
         props.clickedChoice(props.name);
+        setButtonHeight(5);
     }
 
 
-    return <button className={styles.navButton} onClick={clicked}>{props.name}</button>
+    return <button className={styles.navButton} onClick={clicked} style={{height: `${buttonHeight}rem`}}>{props.name}</button>
 
 
 };
