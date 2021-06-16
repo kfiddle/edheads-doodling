4908012d5cfda16b4efdd09443006f895135f345
kfiddle <kenjfiddle@gmail.com>
2021-06-16 16:30:13 -0400

    about us side panel

--- src/components/aboutUsPanel/AboutUsPanel.js
+++ src/components/aboutUsPanel/AboutUsPanel.js
@@ -1,6 +1,7 @@
 
 import { useState, useEffect } from 'react';
 
+import TextBox from '../textBox/TextBox';
 import styles from './AboutUsPanel.module.css';
 
 
@@ -8,7 +9,7 @@ const AboutUsPanel = props => {
     const [slidePosition, setSlidePosition] = useState(100);
 
     useEffect(() => {
-        setTimeout(()=> { setSlidePosition(0) }, 500);
+        setTimeout(()=> { setSlidePosition(0) }, 20);
 
 
     }, [])
@@ -16,7 +17,9 @@ const AboutUsPanel = props => {
 
     return <div className={styles.aboutUsDiv} style={{transform: `translateX(${slidePosition}vw)`}}>
 
-
+    <TextBox className={styles.textBox}><h1>Don't worry, this will have actual content soon.</h1>
+    <p>Plus, it won't be so orange.</p>
+    </TextBox>
 
 
     </div>
