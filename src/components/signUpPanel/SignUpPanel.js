014711d10dcf2f595ae3ad6367e8da40d240123b
kfiddle <kenjfiddle@gmail.com>
2021-06-15 21:37:48 -0400

    sign-up details

--- src/components/signUpPanel/SignUpPanel.js
+++ src/components/signUpPanel/SignUpPanel.js
@@ -1,21 +1,19 @@
-
-import TextBox from '../textBox/TextBox';
-import paulStem from '../../assets/Paul Stem.svg'
-import styles from './SignUpPanel.module.css';
+import TextBox from "../textBox/TextBox";
+import paulStem from "../../assets/Paul Stem.svg";
+import styles from "./SignUpPanel.module.css";
 
 const SignUpPanel = () => {
-
-    return <div className={styles.signUpDiv}>
-
-    <div className={styles.paulStemDiv}>
-    <img src={paulStem} className={styles.paulStem}></img>
-
+  return (
+    <div className={styles.signUpDiv}>
+      <div className={styles.paulStemDiv}>
+        <img src={paulStem} className={styles.paulStem}></img>
+      </div>
+      <TextBox direction={"left"} className={styles.textBox}>
+        <h3>Made for kids, Used by Teachers</h3>
+        <button className={styles.signUpButton}><p className={styles.buttonText}>Sign Up</p></button>
+      </TextBox>
     </div>
-    <TextBox direction={'left'} className={styles.textBox}></TextBox>
-
-    </div>
-
-
+  );
 };
 
 export default SignUpPanel;
