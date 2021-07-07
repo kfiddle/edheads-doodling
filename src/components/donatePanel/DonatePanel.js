d460059623dcf96a7644d3f53d5968324ee6fc29
kfiddle <kenjfiddle@gmail.com>
2021-07-07 16:38:44 -0400

    to match Karin's

--- src/components/donatePanel/DonatePanel.js
+++ src/components/donatePanel/DonatePanel.js
@@ -1,23 +1,51 @@
 import TextBox from "../textBox/TextBox";
-import izzy from "../../assets/Izzy Honda.svg";
+import izzy from "../../assets/robot.PNG";
 import styles from "./DonatePanel.module.css";
 
 const DonatePanel = () => {
   return (
     <div className={styles.donatePanelDiv}>
 
-      <TextBox direction={"right"} className={styles.textBox}>
-        <p className={styles.getInvolved}>GET INVOLVED</p>
+  <div className={styles.donateContainer}>
 
-        <h3>Your support matters</h3>
-        <button className={styles.donateButton}><p className={styles.buttonText}>Donate</p></button>
+      <TextBox direction={"right"} className={styles.textBox}>
+      <p className={styles.aboutUs}>Get Involved</p>
+          <h3 className={styles.welcomeTitle}>
+            Get Involved Title
+          </h3>
+
+          <p className={styles.basicText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          </p>
+          <p className={styles.basicText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper luctus tempus. Nulla facilisi. Proin placerat nulla arcu. 
+          </p>
+
+          <div className={styles.buttonRow}>
+            <button
+              className={styles.buttonToTheGames}
+            >
+              Sign Up
+            </button>
+
+            <div className={styles.discoverUs}>
+
+              <button className={styles.buttonToAboutUs}>
+                Lorem Ipsum!
+              </button>
+
+            </div>
+            
+          </div>
       </TextBox>
 
       <div className={styles.izzyDiv}>
         <img src={izzy} className={styles.izzy}></img>
       </div>
     </div>
+    </div>
   );
 };
 
 export default DonatePanel;
+
--- src/components/signUpPanel/SignUpPanel.js
+++ src/components/signUpPanel/SignUpPanel.js
@@ -5,13 +5,47 @@ import styles from "./SignUpPanel.module.css";
 const SignUpPanel = () => {
   return (
     <div className={styles.signUpDiv}>
+
+<div className={styles.signUpContainer}>
+      
       <div className={styles.paulStemDiv}>
         <img src={paulStem} className={styles.paulStem}></img>
       </div>
+      
       <TextBox direction={"left"} className={styles.textBox}>
-        <h3>Made for kids, Used by Teachers</h3>
-        <button className={styles.signUpButton}><p className={styles.buttonText}>Sign Up</p></button>
+      <p className={styles.aboutUs}>SIGN UP</p>
+          <h3 className={styles.welcomeTitle}>
+            Made for kids; Used by teachers
+          </h3>
+
+          <p className={styles.basicText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          </p>
+          <p className={styles.basicText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper luctus tempus. Nulla facilisi. Proin placerat nulla arcu. 
+          </p>
+
+          <div className={styles.buttonRow}>
+            <button
+              className={styles.buttonToTheGames}
+            >
+              Sign Up
+            </button>
+
+            <div className={styles.discoverUs}>
+
+              <button className={styles.buttonToAboutUs}>
+                Lorem Ipsum!
+              </button>
+
+            </div>
+            
+          </div>
       </TextBox>
+
+      </div>
+
+
     </div>
   );
 };
