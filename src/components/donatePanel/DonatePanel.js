1095ecbc3f097e3ce80aae758445eddb9bca4e3f
kfiddle <kenjfiddle@gmail.com>
2021-07-23 18:57:24 -0400

    various

--- src/components/donatePanel/DonatePanel.js
+++ src/components/donatePanel/DonatePanel.js
@@ -5,44 +5,32 @@ import styles from "./DonatePanel.module.css";
 const DonatePanel = () => {
   return (
     <div className={styles.donatePanelDiv}>
-
-  <div className={styles.donateContainer}>
-
-      <TextBox direction={"right"} className={styles.textBox}>
-      <p className={styles.aboutUs}>Get Involved</p>
-          <h3 className={styles.welcomeTitle}>
-            Get Involved Title
-          </h3>
+      <div className={styles.donateContainer}>
+        <TextBox direction={"right"} className={styles.textBox}>
+          <p className={styles.aboutUs}>Get Involved</p>
+          <h3 className={styles.welcomeTitle}>Get Involved Title</h3>
 
           <p className={styles.basicText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
           <p className={styles.basicText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper luctus tempus. Nulla facilisi. Proin placerat nulla arcu. 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper
+            luctus tempus. Nulla facilisi. Proin placerat nulla arcu.
           </p>
 
           <div className={styles.buttonRow}>
-            <button
-              className={styles.buttonToTheGames}
-            >
-              Sign Up
-            </button>
+            <button className={styles.buttonToTheGames}>Sign Up</button>
 
             <div className={styles.discoverUs}>
-
-              <button className={styles.buttonToAboutUs}>
-                Lorem Ipsum!
-              </button>
-
+              <button className={styles.buttonToAboutUs}>Lorem Ipsum!</button>
             </div>
-            
           </div>
-      </TextBox>
+        </TextBox>
 
-      <div className={styles.izzyDiv}>
-        <img src={izzy} className={styles.izzy}></img>
+        <div className={styles.izzyDiv}>
+          <img src={izzy} className={styles.izzy}></img>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
